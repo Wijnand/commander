5f8b62a2e4fe126cd45ebfc1691d70c1a38cd3bf
Wijnand Wiersma <wijnand@videre.net>
2010-03-18 21:26:15 +0100

    Use the options from the config

--- lib/commander.rb
+++ lib/commander.rb
@@ -1,4 +1,8 @@
-class Commander
-  def self.run
+class Commander < ActiveResource::Base
+  
+  def self.run(config)
+    self.site = config['url']
+    self.user = config['user']
+    self.password = config['pass']
   end
 end
